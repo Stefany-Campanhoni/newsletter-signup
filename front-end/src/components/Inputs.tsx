e3330f142ea56d732227ee6fc1d3f69dc23d84e6
Stefany <scampanhoni@gmail.com>
2023-10-24 22:43:30 -0300

    folder structure

--- front-end/src/components/Inputs.tsx
+++ front-end/src/components/Inputs.tsx
@@ -18,15 +18,27 @@ export default function Inputs() {
   const navigate = useNavigate()
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
-    navigate("success", {state: data.email})
+    navigate("success", { state: data.email })
   }
 
-  const validateEmail = (email: string): boolean => {
+  const validateEmailAndSending = async (
+    emailToTest: string
+  ): Promise<boolean> => {
     const emailRegex: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
 
-    if (!email || !emailRegex.test(email)) {
+    if (!emailToTest || !emailRegex.test(emailToTest)) {
       return false
     }
+
+    try {
+      await fetch("http://127.0.0.1:8080/send-email", {
+        method: "POST",
+        body: JSON.stringify({ email: emailToTest }),
+      })
+    } catch (e) {
+      return false
+    }
+
     return true
   }
 
@@ -45,7 +57,7 @@ export default function Inputs() {
       <Controller
         name="email"
         control={control}
-        rules={{ required: true, validate: validateEmail }}
+        rules={{ required: true, validate: validateEmailAndSending }}
         render={({ field }) => (
           <input
             {...field}
@@ -57,7 +69,10 @@ export default function Inputs() {
         )}
       />
 
-      <Button name="Subscribe to monthly newsletter" type="submit" />
+      <Button
+        name="Subscribe to monthly newsletter"
+        type="submit"
+      />
     </form>
   )
 }
